4bd81ca07b76d9e0682bd40b7646f4bd05da49ff
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-30 12:20:14 +0530

    Objects

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
+//Enum :-
 // // let user: [number, string] = [1, "swastik"]
-
 // const enum Size {Small = 1, Medium, Large};
 // let mySize :Size = Size.Medium
 // console.log(mySize );
 
 
-function calculateTax(income: number, taxYear = 2022):number{
-    if (taxYear < 2022) 
-        return income * 1.2
-    return income * 1.3
+//functions :-
+// function calculateTax(income: number, taxYear = 2022):number{
+//     if (taxYear < 2022) 
+//         return income * 1.2
+//     return income * 1.3
     
-}
+// }
 
-calculateTax(10_999, 2022)
+// calculateTax(10_999, 2022)
+
+
+//Objects :-
+let emplopyee: {
+    id: number,
+    name: string,
+    retire: (Date: Date) => void
+} = {
+    id:1,
+     name: "Swastik",
+     retire: (Date: Date) => {
+        console.log(Date);
+        
+     }
+}
