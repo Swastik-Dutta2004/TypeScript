df32f026490fec217dc70929e15486956c657017
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-30 11:55:11 +0530

    Doing function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-// let user: [number, string] = [1, "swastik"]
+// // let user: [number, string] = [1, "swastik"]
 
-const enum Size {Small = 1, Medium, Large};
-let mySize :Size = Size.Medium
-console.log(mySize );
+// const enum Size {Small = 1, Medium, Large};
+// let mySize :Size = Size.Medium
+// console.log(mySize );
+
+
+function calculateTax(income: number):number{
+    if (income < 50_000) 
+        return income * 1.2
+    return income * 1.2
+    
+}
