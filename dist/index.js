758afff79869fd9dcae1666bbdabbd4a1a681c0c
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-30 10:09:33 +0530

    completed tuples.

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 50;
+if (age > 40) {
+    age += 10;
+    console.log(age);
+}
+//# sourceMappingURL=index.js.map
