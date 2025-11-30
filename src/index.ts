55f0ace62fece87a1a7dbda1a77fe38e0f95a847
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-11-30 12:38:21 +0530

    Type Alliases

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,37 @@
 //     if (taxYear < 2022) 
 //         return income * 1.2
 //     return income * 1.3
-    
+
 // }
 
 // calculateTax(10_999, 2022)
 
 
 //Objects :-
-let emplopyee: {
-    id: number,
+// let employee: {
+//     readonly id: number,
+//     name: string,
+//     retire: (Date: Date) => void
+// } = {
+//     id:1,
+//     name: "Swastik",
+//     retire: (Date: Date) => {
+//         console.log(Date);
+//      }
+// }
+
+// Typr ALLiases
+
+type Employee = {
+    readonly id: number,
     name: string,
     retire: (Date: Date) => void
-} = {
-    id:1,
-     name: "Swastik",
-     retire: (Date: Date) => {
+}
+
+let employee: Employee =  {
+    id: 1,
+    name: "Swastik",
+    retire: (Date: Date) => {
         console.log(Date);
-        
-     }
+    }
 }
