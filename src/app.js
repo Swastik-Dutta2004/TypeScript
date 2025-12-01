4fbd8f75db29089f037bdfa8f62bfc24f6804116
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-12-01 18:26:33 +0530

    rest operate

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-function abcd(name, age, gender) {
-    console.log(name, age, gender);
+function abcd() {
+    var arr = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        arr[_i] = arguments[_i];
+    }
+    console.log(arr);
 }
-abcd("Swastik", 20, "male");
+abcd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-function abcd(name: string, age:number, gender? : string){
-    console.log(name, age, gender);
+function abcd(...arr:number[]){
+    console.log(arr);
     
 }
-abcd("Swastik", 20, "male" )
+abcd(1,2,3,4,5,6,7,8,9,10)
